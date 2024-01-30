6f9abcd95c59d6b1b2e8fe44e7b96e88547fcfb7
Eason Ong <eong@lcs.on.ca>
2024-01-30 11:31:29 -0500

    Initial Commit

---
+++ GuessingGame/GuessingGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessingGameApp.swift
+//  GuessingGame
+//
+//  Created by Ong Eason on 30/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct GuessingGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
