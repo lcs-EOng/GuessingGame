6f61b5359eec65f15317f8456a2ee1c468b61f15
Eason Ong <eong@lcs.on.ca>
2024-01-31 11:41:42 -0500

    Change it so that user can type in values

--- GuessingGame/GameView.swift
+++ GuessingGame/GameView.swift
@@ -12,7 +12,7 @@ struct GameView: View {
     // MARK: Stored properties
     
     // What number the user has guessed
-    @State var selectedNumber = 50
+    @State var givenInput = ""
     
     // What number the computer wants the user to guess
     @State var target = Int.random(in: 1...100)
@@ -34,13 +34,7 @@ struct GameView: View {
                 Text("Guess what it is!")
                     .font(.headline)
                 
-                Stepper(value: $selectedNumber, in: 1...100) {
-                    HStack {
-                        Text("Make a guess:")
-                        Spacer()
-                        Text("\(selectedNumber)")
-                    }
-                }
+                TextField("Make a guess", text: $givenInput)
                 
                 Button {
                     checkGuess()
@@ -95,6 +89,11 @@ struct GameView: View {
     func checkGuess() {
         
         // Provide feedback to the user
+        // Attempt to unwrap the input provided by the user
+        guard let selectedNumber = Int(givenInput) else {
+            feedback = "Please provide an integer."
+            return
+        }
         // When should they guess higher?
         // When should then guess lower?
         // FILL IN THIS CODE
@@ -115,8 +114,8 @@ struct GameView: View {
     // Start a new game
     func reset() {
 
-        // Start the user back at 50
-        selectedNumber = 50
+        // Start the user back with an empty string
+        givenInput = ""
         
         // Have the computer guess a new number
         target = Int.random(in: 1...100)
